26abf0582df3841677a214c17ce8f36372dc08bf
fanimhrn <fanimaharani7@gmail.com>
2017-03-08 18:40:01 +0700

    commit ketmpat
    Signed-off-by: fanimhrn <fanimaharani7@gmail.com>

--- src/se/tan/BasicCrawler.java
+++ src/se/tan/BasicCrawler.java
@@ -46,9 +46,9 @@ public class BasicCrawler {
         new BasicCrawler().getPageLinks("https://www.bagas31.com/2017/03/spotify-music-mod-premium-v7-7-for-android.html");
         new BasicCrawler().getPageLinks("https://www.bagas31.com/2017/03/spotify-music-mod-premium-v7-7-for-android.html");
         new BasicCrawler().getPageLinks("https://www.bagas31.com/2017/03/spotify-music-mod-premium-v7-7-for-android.html");
+        new BasicCrawler().getPageLinks("https://www.bagas31.com/2017/03/spotify-music-mod-premium-v7-7-for-android.html");
 
 
-        // akubusuk
     }
 
 }
